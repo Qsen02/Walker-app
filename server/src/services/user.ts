c91248b9757f9c77e8423866f99fd19451d6e5a8
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-07-06 12:51:49 +0300

    User service implemented

---
+++ server/src/services/user.ts
@@ -0,0 +1,106 @@
+import { Users } from "../models/users";
+import bcrypt from "bcrypt";
+
+async function register(username: string, password: string, email: string) {
+	const isUsernameExist = await Users.findOne({ username: username }).lean();
+	if (isUsernameExist) {
+		throw new Error("User with this username already exist!");
+	}
+	const isEmailExist = await Users.findOne({ email: email }).lean();
+	if (isEmailExist) {
+		throw new Error("User with this email already exist!");
+	}
+
+	const user = await Users.create({
+		username: username,
+		password: await bcrypt.hash(password, 10),
+		email: email,
+	});
+
+	return user;
+}
+
+async function login(username: string, password: string) {
+	const isValidUsername = await Users.findOne({ username: username }).lean();
+	if (!isValidUsername) {
+		throw new Error("Username or password don't match!");
+	}
+	if (!isValidUsername?.password) {
+		return;
+	}
+	const isValidPassword = await bcrypt.compare(
+		password,
+		isValidUsername.password
+	);
+	if (!isValidPassword) {
+		throw new Error("Username or password don't match!");
+	}
+
+	return isValidPassword;
+}
+
+async function getUserById(userId: string) {
+	const user = await Users.findById(userId)
+		.populate("activeDays")
+		.populate("waterDays")
+		.lean();
+	if (!user) {
+		throw new Error("Resource not found!");
+	}
+
+	return user;
+}
+
+async function checkUserId(userId: string) {
+	const user = await Users.findById(userId).lean();
+	if (!user) {
+		return false;
+	}
+
+	return true;
+}
+
+async function editUser(
+	userId: string,
+	newUsername: string,
+	newEmail: string,
+	newPurpose: number
+) {
+	const updatedUser = await Users.findByIdAndUpdate(
+		userId,
+		{
+			$set: {
+				username: newUsername,
+				email: newEmail,
+				purpose: newPurpose,
+			},
+		},
+		{ new: true }
+	).lean();
+
+	return updatedUser;
+}
+
+async function editPassword(userId: string, newPassword: string) {
+	const user = await Users.findById(userId).lean();
+	if (!user?.password) {
+		return;
+	}
+	const isOldPassword = await bcrypt.compare(newPassword, user?.password);
+	if (isOldPassword) {
+		throw new Error("Old password can't be the new password!");
+	}
+	const updatedUser = await Users.findByIdAndUpdate(
+		userId,
+		{
+			$set: {
+				password: await bcrypt.hash(newPassword, 10),
+			},
+		},
+		{ new: true }
+	).lean();
+
+	return updatedUser;
+}
+
+export { register, login, getUserById, checkUserId, editUser, editPassword };
