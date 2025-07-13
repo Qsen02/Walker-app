8e609911271ceb9353dcef6cac8d57f03bc62367
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-07-13 11:43:53 +0300

    Water service implemented

---
+++ client/src/api/waterService.ts
@@ -0,0 +1,19 @@
+import { Water } from "../types/water";
+import { get, post, put } from "./requester";
+
+const endpoint = "/water";
+
+export async function getWaterById(waterId:string){
+    const data = await get(`${endpoint}/${waterId}`);
+    return data as Water;
+}
+
+export async function createWater(){
+    const data = await post(endpoint,{});
+    return data as Water;
+}
+
+export async function addWater(waterId:string,data:object){
+    const updatedData = await put(`${endpoint}/${waterId}`,data);
+    return updatedData as Water;
+}
