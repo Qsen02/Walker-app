fad8d33ebf2bced48753c97457f60193c0caab37
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-07-13 11:37:27 +0300

    Steps service implemented

---
+++ client/src/api/stepsService.ts
@@ -0,0 +1,19 @@
+import { Steps } from "../types/steps";
+import { get, post, put } from "./requester";
+
+const endpoint="/steps";
+
+export async function getStepsById(stpesId:string){
+    const data=await get(`${endpoint}/${stpesId}`);
+    return data as Steps;
+}
+
+export async function createSteps(){
+    const data = await post(endpoint,{});
+    return data as Steps;
+}
+
+export async function incrementSteps(stepsId:string){
+    const updatedData = await put(`${endpoint}/${stepsId}`,{});
+    return updatedData as Steps;
+}
