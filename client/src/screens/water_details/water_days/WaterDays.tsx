7a385e151265f26a3cd84aef5fbc0609e2d67d0a
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-08-09 12:03:00 +0300

    Implemented styling on water days component

--- client/src/screens/water_details/water_days/WaterDays.tsx
+++ client/src/screens/water_details/water_days/WaterDays.tsx
@@ -4,22 +4,32 @@ import {
 	useNavigation,
 	useRoute,
 } from "@react-navigation/native";
-import { Text, TouchableOpacity, View } from "react-native";
+import { ActivityIndicator, Text, TouchableOpacity, View } from "react-native";
 import { Routes } from "../../../types/RoutingTable";
 import { globalStyles } from "../../../../globalStyles";
 import { useUserThemeContext } from "../../../contexts/user_theme_context";
 import Icon from "react-native-vector-icons/FontAwesome6";
 import { useGetLastWater } from "../../../hooks/useUser";
+import { waterDaysStyles } from "./WaterDaysStyles";
 
 export default function WaterDays() {
 	const { theme, userState, language } = useUserThemeContext();
 	const route = useRoute<RouteProp<Routes, "Water">>();
 	const { userId } = route.params;
 	const navigation = useNavigation<NavigationProp<Routes>>();
-    const {waterDays,loading,error}=useGetLastWater([],userId);
+	const { waterDays, loading, error } = useGetLastWater([], userId);
 
 	return (
 		<>
+			{loading && !error ? (
+				<ActivityIndicator
+					size={60}
+					color="rgba(6,173,0,1)"
+					style={globalStyles.spinner}
+				/>
+			) : (
+				""
+			)}
 			<TouchableOpacity
 				style={globalStyles.arrowButton}
 				onPress={() => navigation.goBack()}
@@ -30,19 +40,44 @@ export default function WaterDays() {
 					size={25}
 				/>
 			</TouchableOpacity>
-			<View>
-				<Text
-					style={
+			{error ? (
+				<View
+					style={[
+						globalStyles.errorContainer,
 						theme == "light"
-							? { color: "black" }
-							: { color: "white" }
-					}
+							? globalStyles.whiteThemeNormal
+							: globalStyles.darkThemeNormal,
+					]}
 				>
-					{language == "english"
-						? "The last 7 days you drank water"
-						: "Последните 7 дни в които сте пили вода"}
-				</Text>
-			</View>
+					<Text
+						style={[
+							globalStyles.errorText,
+							theme == "light"
+								? { color: "black" }
+								: { color: "white" },
+						]}
+					>
+						{language == "english"
+							? "Server is not responding, please try again later!"
+							: "Сървърът не отговаря, моля опитайте по късно!"}
+					</Text>
+				</View>
+			) : (
+				<View style={waterDaysStyles.wrapper}>
+					<Text
+						style={[
+							theme == "light"
+								? { color: "black" }
+								: { color: "white" },
+							waterDaysStyles.title,
+						]}
+					>
+						{language == "english"
+							? "The last 7 days you drank water"
+							: "Последните 7 дни в които сте пили вода"}
+					</Text>
+				</View>
+			)}
 		</>
 	);
 }
---
+++ client/src/screens/water_details/water_days/WaterDaysStyles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const waterDaysStyles=StyleSheet.create({
+    wrapper:{
+        marginTop:20,
+        width:"100%",
+        display:"flex",
+        flexDirection:"column",
+        gap:20,
+        alignItems:"center"
+    },
+    title:{
+        fontSize:18,
+        textAlign:"center",
+        fontWeight:"bold"
+    }
+})
